c55a6766815a65dcfad4f964d8f0d42edca1cf62
mikima25 <vtalism20@gmail.com>
2017-03-05 22:41:59 +0300

    submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,18 @@
 var player1 = new Player("playerOne");
-var player2 = new Player("playerTwo")
+var player2 = new Player("playerTwo");
+
+function Player(name) {
+  this.name = name;
+  this.score = 0;
+}
+Player.prototype.increaseScore = function(score) {
+  this.score += score;
+}
+var dieFace = function() {
+  return Math.floor((Math.random() * 6) + 1);
+};
+
+//front-end
+$(document).ready(function() {
+    
+}
